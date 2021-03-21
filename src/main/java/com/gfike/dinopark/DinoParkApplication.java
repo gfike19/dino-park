f004f9d6b14fc315bbb7713b2b7628bee6e0cc13
Gail <gfike19@gmail.com>
2021-03-21 14:51:59 -0500

    first commit

---
+++ src/main/java/com/gfike/dinopark/DinoParkApplication.java
@@ -0,0 +1,13 @@
+package com.gfike.dinopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinoParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinoParkApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/gfike/dinopark/controllers/MainController.java
@@ -0,0 +1,4 @@
+package com.gfike.dinopark.controllers;
+
+public class MainController {
+}
