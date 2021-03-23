e1f6f61cac8f69859ee8acc45cd59b104abe79a9
Gail <gfike19@gmail.com>
2021-03-23 16:13:35 -0500

    started work on being able to display dino stats after user selects one from the dropdown

--- src/main/java/com/gfike/dinopark/controllers/MainController.java
+++ src/main/java/com/gfike/dinopark/controllers/MainController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -18,11 +21,37 @@ public class MainController {
     DinoDao dinoDao;
 
     @GetMapping
-    public String index(Model model) {
+    public String index(Model model, HttpSession session) {
+        boolean dinoSelected;
+        if(session.getAttribute("dinoSelected") == null) {
+            dinoSelected = false;
+            session.setAttribute("dinoSelected", dinoSelected);
+        } else {
+            dinoSelected = (boolean) session.getAttribute("dinoSelected");
+        }
+        model.addAttribute("dinoSelected", dinoSelected);
         model.addAttribute("allDinos", dinoDao.findAll());
+        model.addAttribute("title", "Jurassic World Evolution Helper App");
         return "index";
     }
 
+    @PostMapping
+    public String index(Model model, ServletRequest request, HttpSession session) {
+        try {
+            String id = request.getParameter("allDinos");
+            Dino currDino = dinoDao.findById(Integer.parseInt(id));
+            model.addAttribute("currDino", currDino);
+            boolean dinoSelected;
+            session.removeAttribute("dinoSelected");
+            dinoSelected = true;
+            session.setAttribute("dinoSelected", dinoSelected);
+            model.addAttribute("dinoSelected", dinoSelected);
+        } catch(Exception e) {
+            String msg = e.toString();
+            model.addAttribute("msg",  msg);
+        }
+        return "redirect:/";
+    }
     @GetMapping(path="error")
     public String Error () {
         return "error";
--- src/main/java/com/gfike/dinopark/models/Dino.java
+++ src/main/java/com/gfike/dinopark/models/Dino.java
@@ -56,6 +56,10 @@ public class Dino {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getDinoName() {
         return dinoName;
     }
